268147854ec68faddc7b1050b4266de3c905e17e
Manav Malhotra <manavmalhotra173@gmail.com>
2023-08-27 14:29:42 +0530

    Create lab8q6.java

---
+++ Lab 8/lab8q6.java
@@ -0,0 +1,17 @@
+/* Write a java program to justify “A subclass exception must appear before super-class exception”.  */
+
+public class lab8q6 {
+    public static void main(String[] args) {
+        try{
+            int result = 5/0;
+            System.out.println("Result is: "+result);
+        }
+        catch(ArithmeticException e2){
+            System.out.println("Subclass ArithmeticException Occured");
+        }
+        catch(Exception e1){
+            System.out.println("Superclass Exception Occured");
+        }
+        System.out.println("Rest of the Code");
+    }
+}
